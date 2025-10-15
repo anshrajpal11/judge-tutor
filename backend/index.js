99112f87ec5701901531f7a01475d526b91b5b81
anshrajpal11 <rajpalansh11@example.com>
2025-10-15 16:36:41 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+    "https://judge-tutor.vercel.app"],
     credentials: true,
   })
 );
